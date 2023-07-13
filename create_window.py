0ba2567921a8c5832f9a8e1268ba2bc65e154ac8
Rhys Sorenson-Graff <127057159+Rhys-sg@users.noreply.github.com>
2023-07-13 16:37:37 -0700

    No longer used

--- create_window.py
+++
@@ -1,8 +0,0 @@
-import graphics
-
-def create_window(boarder_width, boarder_height, window_width, window_height):
-    window = graphics.GraphWin("Screen", window_width, window_height)
-    bounds = graphics.Rectangle(graphics.Point((window_width / 2) - (boarder_width / 2), (window_height / 2) + (boarder_height / 2)),
-                                graphics.Point((window_width / 2) + (boarder_width / 2), (window_height / 2) - (boarder_height / 2)))
-    bounds.draw(window)
-    return window
