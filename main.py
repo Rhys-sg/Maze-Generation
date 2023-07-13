2e862e33d2781f87029a0bd19f99a009ba9bf484
Rhys Sorenson-Graff <127057159+Rhys-sg@users.noreply.github.com>
2023-07-13 16:38:09 -0700

    Complete main.py

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-import DFS
-import create_dict_cell_walls
+import algorithm
+import grid_constructor
 import graphics
 
-
-grid_width = 30
-grid_height = 30
-grid_cell_side_length = 10
-grid = [[{"pos" : (x, y)} for y in range(grid_height)] for x in range(grid_width)]
-
-boarder_width = grid_cell_side_length * grid_width
-boarder_height = grid_cell_side_length * grid_height
-window_width = 720
-window_height = 540
-
-window = graphics.GraphWin("Screen", window_width, window_height)
+from global_variables import *
 
 def main():
-    create_dict_cell_walls.create_dict_cell_walls(grid, window, window_width, window_height, boarder_width, boarder_height, grid_cell_side_length)
+    window = graphics.GraphWin("Screen", WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    grid = grid_constructor.grid(window)
+
+    algorythm.findPath(grid[0][0], grid)
 
     # pause for click in window, then closes
     window.getMouse()
