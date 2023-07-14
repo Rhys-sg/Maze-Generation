518399ffd88008f30a4301f90e01d7e134c500f9
Rhys Sorenson-Graff <127057159+Rhys-sg@users.noreply.github.com>
2023-07-13 19:08:17 -0700

    Swap width and height

--- neighbors.py
+++ neighbors.py
@@ -8,12 +8,12 @@ def _findNeighbors(cell, grid):
     cell.update({"right_cell" : None})
     cell.update({"left_cell" : None})
 
-    # if the adjacent cell is in bounds (i.e., the current cell is not an edge), update adjacent cells
-    if (cell.get("y") < GRID_WIDTH-1):
+    # if the adjacent cell is in bounds (i.e. the current cell is not an edge), update adjacent cells
+    if (cell.get("y") < GRID_HEIGHT-1):
         cell.update({"top_cell" : grid[cell.get("x")][cell.get("y")+1]})
     if (cell.get("y") > 0):
         cell.update({"bottom_cell" : grid[cell.get("x")][cell.get("y")-1]})
-    if (cell.get("x") < GRID_HEIGHT-1):
+    if (cell.get("x") < GRID_WIDTH-1):
         cell.update({"right_cell" : grid[cell.get("x")+1][cell.get("y")]})
     if (cell.get("x") > 0):
         cell.update({"left_cell" : grid[cell.get("x")-1][cell.get("y")]})
